4a1fcc173016d079e1569c44a5e55a0b26c11d1a
Naincy Gupta <nansee1804@gmail.com>
2018-02-04 14:11:45 +0530

    Clean up as per requirement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,26 +13,13 @@ import { LbdModule } from './lbd/lbd.module';
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
 import { TablesComponent } from './tables/tables.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent,
-    TablesComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent
-
+    TablesComponent
   ],
   imports: [
     BrowserModule,
