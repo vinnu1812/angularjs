058bcc80845e785be2ea62857fc525c297024f39
Naincy Gupta <nansee1804@gmail.com>
2018-02-24 15:53:47 +0530

    Implemented new column status

--- src/app/health/health.component.ts
+++ src/app/health/health.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Http} from "@angular/http";
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
+import { config } from '../config/config';
 
 @Component({
   selector: 'app-tables',
@@ -15,8 +16,9 @@ export class HealthComponent implements OnInit {
     public rowsOnPage = 5;
     public sortBy = "host";
     public sortOrder = "asc";
-
+    public status = "";
     constructor(private http: Http) {
+        console.log(config.apiUrl);
     }
 
     ngOnInit(): void {
@@ -24,12 +26,13 @@ export class HealthComponent implements OnInit {
     }
 
     public getData() {
-        this.http.get("assets/dummy.json")
+        this.http.get(config.apiUrl)
         .subscribe((data)=> {
             setTimeout(()=> {
                 this.data = data.json();
                 this.healthData = new Array<any>();
-                this.healthData = this.healthData.concat(this.data.healthMetrics);
+                this.healthData = this.healthData.concat(this.data.HealthMetrics);
+                //fomatHealthData
             }, 1000);
         });
     }
@@ -53,5 +56,16 @@ export class HealthComponent implements OnInit {
             this.healthData,
             'HealthData',
             { headers: Object.keys(this.healthData[0]) });
+    }
+
+    public getStatus(timestamp : string){
+        var dataDate  = new Date(timestamp);
+        var threeDayFromCurrentDate = new Date();
+        threeDayFromCurrentDate.setDate( threeDayFromCurrentDate.getDate() + 3 );
+        if (dataDate > threeDayFromCurrentDate) {
+            return "RED"
+        } else {
+            return  "GREEN";
+        }    
     }
 }
